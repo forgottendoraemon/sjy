039413f4844b9828dea78390042b48e7139427a8
gs <gs@ex.com>
2019-08-04 16:41:32 +0800

    geojson2mvt

---
+++ geojson2mvt/start.js
@@ -0,0 +1,30 @@
+var fs = require('fs');
+var geojson2mvt = require('./index');
+const path = require('path');
+
+const geojsondir = path.join(__dirname, '../static/mapdata');
+const files = fs.readdirSync(geojsondir);
+const geojson_files = files.filter((f) => f.endsWith('.geojson'));
+
+const options = {
+    layers:{},
+    rootDir: path.join(__dirname, '../static/tiles'),// 输出目录z/x/y
+    bbox: [
+        31.5989969849999994,
+        89.4044260799999932,
+        39.2131070100000017,
+        103.0710229200000043
+    ], //[miny,minx,maxy,maxx]
+    zoom: {
+        min: 5,
+        max: 18
+    }
+}
+
+for (let f of geojson_files) {
+    options.layers[path.parse(f).name] = JSON.parse(fs.readFileSync(`${geojsondir}/${f}`, "utf8"));
+}
+
+geojson2mvt(options);
+
+// 若提示内存超出，可以尝试使用 node –max_old_space_size=4096 start.js
